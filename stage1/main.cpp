955ceeca96dba43f6e3f2a721b692b88c3c98f3f
varun <varun.nalwa@gmail.com>
2021-02-13 13:52:38 +0530

    Initial commit

---
+++ stage1/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 13, 2021
+ *      Author: varun
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+
+
+
