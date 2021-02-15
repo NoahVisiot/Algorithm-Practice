e64e688739955fd6dd5933dd3c71702939cd62ea
Varun Choudhary <varchoud@cisco.com>
2021-02-15 14:04:36 +0530

    Changes made in Makefile for OS Detection

--- stage1/inc.mk
+++ stage1/inc.mk
@@ -1,35 +1,46 @@
-ifndef LOCAL_HOST
-  LOCAL_HOST:=$(shell uname)
-
-  ifeq (Cygwin, $(LOCAL_HOST))
-    LOCAL_HOST:=window
-  endif
-
-  ifeq (GNU/Linux, $(LOCAL_HOST))
-    LOCAL_HOST:=linux
-  endif
-  
-  ifeq (Darwin, $(LOCAL_HOST))
-    LOCAL_HOST:=mac
-  endif
-
+ifeq ($(OS),Windows_NT)
+    CCFLAGS += -D WIN32
+    ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
+        CCFLAGS += -D AMD64
+    else
+        ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
+            CCFLAGS += -D AMD64
+        endif
+        ifeq ($(PROCESSOR_ARCHITECTURE),x86)
+            CCFLAGS += -D IA32
+        endif
+    endif
+else
+    UNAME_S := $(shell uname -s)
+    ifeq ($(UNAME_S),Linux)
+        CCFLAGS += -D LINUX
+         LOCAL_HOST:=linux
+    endif
+    ifeq ($(UNAME_S),Darwin)
+        CCFLAGS += -D OSX
+        LOCAL_HOST:=mac
+    endif
+    UNAME_P := $(shell uname -p)
+    ifeq ($(UNAME_P),x86_64)
+        CCFLAGS += -D AMD64
+    endif
+    ifneq ($(filter %86,$(UNAME_P)),)
+        CCFLAGS += -D IA32
+    endif
+    ifneq ($(filter arm%,$(UNAME_P)),)
+        CCFLAGS += -D ARM
+    endif
 endif
 
 ifeq (linux, $(LOCAL_HOST))
 CC = gcc
 AR = ar
 CXX = g++
-endif
-
-CFLAG = -Wall
-
-ifeq (window, $(LOCAL_HOST))
+else ifeq (window, $(LOCAL_HOST))
 CC = gcc
 CXX = g++
 AR = ar
-endif
-
-ifeq (mac, $(LOCAL_HOST))
+else ifeq (mac, $(LOCAL_HOST))
 CC = gcc
 AR = ar
 CXX = g++
